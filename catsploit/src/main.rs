d04ee8b4d583e853b5e3a13fc8b72720d7f260a4
Mark Ruddy <1markruddy@gmail.com>
2023-02-02 12:35:33 +0000

    implementation for printing exploit information

--- catsploit/src/main.rs
+++ catsploit/src/main.rs
@@ -1,13 +1,17 @@
-use log::error;
+use log::{error, info};
 use std::time::Duration;
 
 use catsploit_lib::core::exploit::remote_tcp::RemoteTcp;
 use catsploit_lib::core::exploit::Exploit;
 use catsploit_lib::module::exploit::ftp::vsftpd_234_backdoor::Vsftpd234Backdoor;
 
+mod cli;
+
 fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+    cli::show_exploits();
 
+    /*
     println!("lets exploit vsftpd v2.3.4");
     println!(
         "Vsftpd exploit description: {}",
@@ -28,4 +32,7 @@ fn main() {
             error!("{}", e);
         }
     }
+    */
+
+    // TODO: basic CLI interface, lets just code, can refactor later
 }
